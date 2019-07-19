04d7f594545b4d0e20a8afeb8e7149ff7e665662
Nick Jaremek <nick13jaremek@gmail.com>
2019-07-19 11:34:55 +0200

    Added Footer component to App component.

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Header from '../Header';
+import Footer from '../Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Header />
+      <Footer />
       </div>
     );
   }
---
+++ src/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <ul>
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
