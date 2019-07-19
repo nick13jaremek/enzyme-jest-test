8579ed2ceedfce8401de7e27e189343c49a2ccc0
Nick Jaremek <nick13jaremek@gmail.com>
2019-07-19 12:05:42 +0200

    Finished shallow tests for App component.

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../src/App';
+import Header from '../src/Header';
+import Footer from '../src/Footer';
 
 describe('App component', () => {
   let wrapper;
@@ -12,6 +14,23 @@ describe('App component', () => {
 
     test('should have mounted the component', () => {
      expect(wrapper).toBeDefined(); 
+    });
+
+    test('should have a Header component', () => {
+      expect(wrapper.find(Header)).toHaveLength(1);
+    });
+
+    test('should have a Footer component', () => {
+      expect(wrapper.find(Footer)).toHaveLength(1);
+    });
+
+
+    test('should not find the header component class name', () => {
+      expect(wrapper.find('.header')).toHaveLength(0);
+    });
+
+    test('should not find the footer component class name', () => {
+      expect(wrapper.find('.footer')).toHaveLength(0);
     });
   });
 });
